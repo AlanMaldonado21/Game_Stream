6aef0cf20905ffe61d68fe734a3d67abd96ef9a9
Alan Maldonado <maldonadoalanivan@gmail.com>
2022-10-11 15:23:48 -0300

    Initial Commit

---
+++ Game_Stream/Game_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_StreamApp.swift
+//  Game_Stream
+//
+//  Created by Alan on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Game_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
